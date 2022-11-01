5001a3d6f8e774934f169b95e29f0d18699ff43d
Ben <ben@yocto.nu>
2022-11-01 21:30:12 +0100

    Fix broken nameserver group

--- src/Objects/Resources/NSGroup.php
+++ src/Objects/Resources/NSGroup.php
@@ -15,18 +15,22 @@ class NSGroup{
 	 * @param Details $details
 	 * @return NSGroup[]|NSGroup
 	 */
-	public static function fromDetails($details,$isSingle=true){
-		$nsgroups = $details->xpath('NSGroup');
+	public static function fromDetails($details,$isSingle=false){
+		$nsgroups = $details->xpath('nsgroup');
 		if($isSingle){
             $nsgroups = [$details];
 		}
 		$data = [];
 		/**@var SimpleXMLElement $nsgroup*/
 		foreach($nsgroups AS $nsgroup){
+            $nameservers = $nsgroup;
+            if(!$isSingle){
+                $nameservers = $nsgroup->xpath('nameservers')[0];
+            }
 			$d = new self;
 			$d->handle					= ((string) @$nsgroup->xpath('handle')[0]) ?? null;
 			$d->alias					= ((string) @$nsgroup->xpath('alias')[0]) ?? null;
-			$d->nameservers				= (NSGroup_Nameservers::from($nsgroup)) ?? null;
+			$d->nameservers				= (NSGroup_Nameservers::from($nameservers)) ?? null;
 			$data[] = $d;
 		}
 		if($isSingle){
